3baddf9c8237f41634a731bc576010d9e18002b6
Stanley <softswagger3155@gmail.com>
2023-03-14 14:42:39 +0800

    上传平台架构SolveWare Service

---
+++ SolveWare_Service_Core/Attributes/ResourceBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveWare_Service_Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class ResourceBase : Attribute
+    {
+        public string Name { get; set; }
+        public ResourceBase(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ SolveWare_Service_Core/Base/Abstract/ElementBase.cs
@@ -0,0 +1,28 @@
+﻿using SolveWare_Service_Core.Base.Interface;
+using SolveWare_Service_Core.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveWare_Service_Core.Base.Abstract
+{
+    public class ElementBase : IElement
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public long Id { get; private set; }
+
+        public ElementBase()
+        {
+            if(Id == 0)
+                Id = IdentityGenerator.IG.GetIdentity();
+        }
+
+        public virtual void UpdateContent()
+        {
+
+        }
+    }
+}
