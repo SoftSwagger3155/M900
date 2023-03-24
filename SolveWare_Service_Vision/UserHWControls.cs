0d595e7c0c3b82d0699620284cc6fff4392d379c
杨辉华 <15116551434@163.com>
2023-03-24 08:48:19 +0800

    Add VisionUI

---
+++ SolveWare_Service_Vision/UserHWControls.cs
@@ -0,0 +1,22 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolveWare_Service_Vision
+{
+    public partial class UserHWControls : UserControl
+    {
+        public UserHWControls()
+        {
+            InitializeComponent();
+        }
+    }
+}
