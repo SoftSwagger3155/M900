71e150adde13da6c7d1584e7c8f534b3c37ffc3a
杨辉华 <15116551434@163.com>
2023-03-27 13:18:35 +0800

    111

---
+++ MF900_SolveWare/Views/AxisMesForm/AxisDebugForm.cs
@@ -0,0 +1,71 @@
+﻿using MF900_SolveWare.Resource;
+using SolveWare_Service_Core;
+using SolveWare_Service_Tool.Motor.Base.Abstract;
+using SolveWare_Service_Tool.Motor.Business;
+using SolveWare_Service_Tool.Motor.Data;
+using SolveWare_Service_Utility.Extension;
+using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MF900_SolveWare
+{
+    public partial class AxisDebugForm : UIForm
+    {
+        Dictionary<string, AxisBase> keyAxis;
+
+        public AxisDebugForm()
+        {
+            InitializeComponent();
+            keyAxis = new Dictionary<string, AxisBase>()
+            {
+                {ResourceKey.Motor_Btm_T, ResourceKey.Motor_Btm_T.GetAxisBase() },
+                {"2",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"3",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"4",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"5",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"6",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"7",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"8",new Motor_Zmcaux(new ConfigData_Motor()) },
+                {"9",new Motor_Zmcaux(new ConfigData_Motor()) },
+            };
+
+            InitalAxisVel();
+            ResourceKey.Op_TowerLight_Green.GetAxisBase();
+            List<string> names = SolveWare.Core.MMgr.Get_Single_Tool_Resource(SolveWare_Service_Core.Definition.Tool_Resource_Kind.Motor).Get_All_Item_Name().ToList();
+        }
+
+        private void InitalAxisMes()
+        {
+            
+
+        }
+        private void InitalAxisVel()
+        {
+            var axisArry = keyAxis.Keys.ToArray();
+            for (int i = 0; i < 4; i++)
+            {
+                uiDataGridView1.Rows.Add(new object[] {axisArry[i], keyAxis[axisArry[i]].MtrSpeed.Jog_Max_Velocity,
+                keyAxis[axisArry[i]].MtrSpeed.Jog_Min_Velocity,keyAxis[axisArry[i]].MtrSpeed.Jog_Acceleration,keyAxis[axisArry[i]].MtrSpeed.Jog_Deceleration});
+            }
+            for (int i = 4; i < 8; i++)
+            {
+                uiDataGridView5.Rows.Add(new object[] {axisArry[i], keyAxis[axisArry[i]].MtrSpeed.Jog_Max_Velocity,
+                keyAxis[axisArry[i]].MtrSpeed.Jog_Min_Velocity,keyAxis[axisArry[i]].MtrSpeed.Jog_Acceleration,keyAxis[axisArry[i]].MtrSpeed.Jog_Deceleration});
+            }
+            uiDataGridView6.Rows.Add(new object[] {axisArry[8], keyAxis[axisArry[8]].MtrSpeed.Jog_Max_Velocity,
+                keyAxis[axisArry[8]].MtrSpeed.Jog_Min_Velocity,keyAxis[axisArry[8]].MtrSpeed.Jog_Acceleration,keyAxis[axisArry[8]].MtrSpeed.Jog_Deceleration});
+        }
+        private void InitalAxisJop()
+        {
+
+        }
+    }
+}
---
+++ MF900_SolveWare/Views/AxisMesForm/IODebugForm.designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace MF900_SolveWare
+{
+    partial class IODebugForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiTableLayoutPanel1 = new Sunny.UI.UITableLayoutPanel();
+            this.uiGroupBox1 = new Sunny.UI.UIGroupBox();
+            this.uiGroupBox2 = new Sunny.UI.UIGroupBox();
+            this.uiTableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uiTableLayoutPanel1
+            // 
+            this.uiTableLayoutPanel1.ColumnCount = 1;
+            this.uiTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.Controls.Add(this.uiGroupBox1, 0, 0);
+            this.uiTableLayoutPanel1.Controls.Add(this.uiGroupBox2, 0, 1);
+            this.uiTableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiTableLayoutPanel1.Location = new System.Drawing.Point(0, 35);
+            this.uiTableLayoutPanel1.Name = "uiTableLayoutPanel1";
+            this.uiTableLayoutPanel1.RowCount = 2;
+            this.uiTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.Size = new System.Drawing.Size(915, 550);
+            this.uiTableLayoutPanel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiTableLayoutPanel1.TabIndex = 0;
+            this.uiTableLayoutPanel1.TagString = null;
+            // 
+            // uiGroupBox1
+            // 
+            this.uiGroupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiGroupBox1.FillColor = System.Drawing.Color.White;
+            this.uiGroupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiGroupBox1.Location = new System.Drawing.Point(4, 5);
+            this.uiGroupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiGroupBox1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiGroupBox1.Name = "uiGroupBox1";
+            this.uiGroupBox1.Padding = new System.Windows.Forms.Padding(0, 28, 0, 0);
+            this.uiGroupBox1.Size = new System.Drawing.Size(907, 265);
+            this.uiGroupBox1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiGroupBox1.StyleCustomMode = true;
+            this.uiGroupBox1.TabIndex = 0;
+            this.uiGroupBox1.Text = "输入IO";
+            this.uiGroupBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiGroupBox1.TitleTop = 12;
+            this.uiGroupBox1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiGroupBox2
+            // 
+            this.uiGroupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiGroupBox2.FillColor = System.Drawing.Color.White;
+            this.uiGroupBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiGroupBox2.Location = new System.Drawing.Point(4, 280);
+            this.uiGroupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiGroupBox2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiGroupBox2.Name = "uiGroupBox2";
+            this.uiGroupBox2.Padding = new System.Windows.Forms.Padding(0, 32, 0, 0);
+            this.uiGroupBox2.Size = new System.Drawing.Size(907, 265);
+            this.uiGroupBox2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiGroupBox2.StyleCustomMode = true;
+            this.uiGroupBox2.TabIndex = 0;
+            this.uiGroupBox2.Text = "输出IO";
+            this.uiGroupBox2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiGroupBox2.TitleTop = 12;
+            this.uiGroupBox2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // IODebugForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(915, 585);
+            this.Controls.Add(this.uiTableLayoutPanel1);
+            this.Name = "IODebugForm";
+            this.RectColor = System.Drawing.Color.DarkSlateGray;
+            this.Style = Sunny.UI.UIStyle.Custom;
+            this.StyleCustomMode = true;
+            this.Text = "IO信息";
+            this.TitleColor = System.Drawing.Color.DarkSlateGray;
+            this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 800, 450);
+            this.uiTableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UITableLayoutPanel uiTableLayoutPanel1;
+        private Sunny.UI.UIGroupBox uiGroupBox1;
+        private Sunny.UI.UIGroupBox uiGroupBox2;
+    }
+}
